bc57e4b12b62cedaf6f2a5ce6516c97eadbd086a
Chris Franklin <cf73021@gmail.com>
2019-02-06 18:39:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ function createRecipe() {
   document.getElementById("main").innerHTML = template(recipe);
 }
 
-function updateRecipe(){
+ function updateRecipe() {
   var recipe = getRecipeVals();
   var recipeTemplate = document.getElementById("recipe-template").innerHTML;
   var template = Handlebars.compile(recipeTemplate);
   document.getElementById("main").innerHTML = template(recipe);
 }
 
-function displayEditForm(){
+ function displayEditForm() {
   var name = document.getElementById("nameHeader").innerHTML;
   var description = document.getElementById("recipeDescription").innerHTML;
   var ingredientNodes = document.getElementsByName("ingredientsList");
   var ingredients = [];
-  for (var i=0; i<ingredientNodes.length;i++){
+  for (var i=0;i<ingredientNodes.length;i++) {
     ingredients.push(ingredientNodes[i].innerText);
   }
   var recipe = {name, description, ingredients, submitAction: 'createRecipe()'};
@@ -26,10 +26,10 @@ function displayEditForm(){
   document.getElementById("main").innerHTML = template(recipe);
 }
 
-function getRecipeVals(){
+ function getRecipeVals() {
   var ingredientNodes = document.getElementsByName("ingredients");
   var ingredients = [];
-  for (var i=0;i<ingredientNodes.length;i++){
+  for (var i=0;i<ingredientNodes.length;i++) {
     if(ingredientNodes[i].value !== "") {
       ingredients.push(ingredientNodes[i].value);
     }
@@ -37,25 +37,22 @@ function getRecipeVals(){
   var name = document.getElementById("name").value;
   var description = document.getElementById("description").value;
   var recipe = {name, ingredients, description};
-  return recipe
+  return recipe;
 }
 
-function handlebarsSetup() {
-  Handlebars.registerHelper('displayIngredient', function(ingredient){
+ function handlebarsSetup() {
+  Handlebars.registerHelper('displayIngredient', function(ingredient) {
     return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
   })
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
   Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
 }
 
-function initForm(){
+ function initForm() {
   var template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
-  document.getElementById("main").innerHTML = template({'submitAction' : 'createRecipe()'})
+  document.getElementById("main").innerHTML = template({'submitAction': 'createRecipe()'});
 }
-function init() {
-  handlebarsSetup();
-  initForm();
-}
+
 
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
