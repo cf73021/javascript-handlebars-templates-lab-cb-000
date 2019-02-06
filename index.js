f94580bc39d27c0e26293a878e9e1b580ec24a00
Chris Franklin <cf73021@gmail.com>
2019-02-06 18:28:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ function updateRecipe(){
   document.getElementById("main").innerHTML = template(recipe);
 }
 
+function displayEditForm(){
+  var name = document.getElementById("nameHeader").innerHTML;
+  var description = document.getElementById("recipeDescription").innerHTML;
+  var ingredientNodes = document.getElementsByName("ingredientsList");
+  var ingredients = [];
+  for (var i=0; i<ingredientNodes.length;i++){
+    ingredients.push(ingredientNodes[i].innerText);
+  }
+}
+
 function init() {
   handlebarsSetup();
   initForm();
