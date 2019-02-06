625b1848b6a82ede2ab79d92b3a6600694994e7a
Chris Franklin <cf73021@gmail.com>
2019-02-06 18:25:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+function createRecipe() {
+  var recipe = getRecipeVals();
+  var recipeTemplate = document.getElementById("recipe-template").innerHTML;
+  var template = Handlebars.compile(recipeTemplate);
+  document.getElementById("main").innerHTML = template(recipe);
+}
+
 function init() {
   handlebarsSetup();
   initForm();
